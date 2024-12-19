6b0deefd93e87968e0d9335a2dac06fea635a4f5
kaypufff <kiara.jimenez2@upr.edu>
2024-12-19 19:36:21 -0400

    Initialize Pygame and create basic game loop

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-print("Hi")
+import pygame
+
+# initializes pygame
+pygame.init()
+
+main_screen = pygame.display.set_mode((800, 600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT :
+            running = False
