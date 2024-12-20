fc975f505a23681e62408f504235141d446121b3
kaypufff <kiara.jimenez2@upr.edu>
2024-12-19 20:03:21 -0400

    Background color and Game title

--- Main.py
+++ Main.py
@@ -5,9 +5,15 @@ pygame.init()
 
 main_screen = pygame.display.set_mode((800, 600))
 
+# Title
+pygame.display.set_caption("MultiPlay-Py")
+
 # Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT :
             running = False
+    # Main Screen Background Color
+    main_screen.fill((0, 0, 0))
+    pygame.display.update()
